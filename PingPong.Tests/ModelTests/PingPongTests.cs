3f5c6819861267bbf04174e2ad2ba8f46ba95c2b
Hristo Dzhambov and Jieun Kang <jieun@>
2020-03-02 14:20:43 -0800

    add test logic.

--- PingPong.Tests/ModelTests/PingPongTests.cs
+++ PingPong.Tests/ModelTests/PingPongTests.cs
@@ -1,14 +1,36 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using PingPong;
+using PingPong.Models;
 
-namespace PingPong.Tests{
+namespace PingPong.Tests
+{
   [TestClass]
   public class CheckPingPongTests
   {
     [TestMethod]
-    public void CheckNumber()
+    public void CheckNumber_CreateInstanceOfCheckPingPong_CheckPingPong()
     {
-      
+      CheckPingPong newPingPong = new CheckPingPong("1");
+      Assert.AreEqual(typeof(CheckPingPong), newPingPong.GetType());
+    }
+
+    [TestMethod]
+    public void CheckNumber_UserInputIsDivisibleBy3_True()
+    {
+      CheckPingPong newPingPong = new CheckPingPong("6");
+      Assert.AreEqual(true, newPingPong.CheckNumber("6"));
+    }
+
+    [TestMethod]
+    public void CheckNumber_UserInputIsDivisibleBy5_True()
+    {
+      CheckPingPong newPingPong = new CheckPingPong("10");
+      Assert.AreEqual(true, newPingPong.CheckNumber("10"));
+    }
+
+    public void CheckNumber_UserInputIsDivisibleBy3and5_True()
+    {
+      CheckPingPong newPingPong = new CheckPingPong("15");
+      Assert.AreEqual(true, newPingPong.CheckNumber("15"));
     }
   }
 }
