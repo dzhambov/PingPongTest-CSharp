35a1811e0c2ee9084e2d135b0dbb0f34d5bba00a
Hristo Dzhambov and Jieun Kang <hristo+jieun@>
2020-03-02 14:24:14 -0800

    add compleete test for PingPong.

--- PingPong/Models/PingPong.cs
+++ PingPong/Models/PingPong.cs
@@ -2,9 +2,31 @@ namespace PingPong.Models
 {
   public class CheckPingPong
   {
-    public bool CheckNumber(int inputNumber)
+    public string UserInput {get;}
+
+    public CheckPingPong(string userInput)
     {
+      UserInput = userInput;
+    }
+    
+    public bool CheckNumber(string userInput)
+    {
+      int userInputNumber = int.Parse(userInput);
       
+      if (userInputNumber % 3 == 0 && userInputNumber % 5 == 0)
+      {
+        return true;
+      }
+
+      else if (userInputNumber % 3 == 0)
+      {
+        return true;
+      }
+      else if (userInputNumber % 5 == 0)
+      {
+        return true;
+      }
+      return false;
     }
   }
 }
